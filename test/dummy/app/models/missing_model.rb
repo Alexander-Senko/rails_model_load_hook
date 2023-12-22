eddc9baae75fc83dd3f4100a9e4b73444a4966d5
Alexander Senko <Alexander.Senko@gmail.com>
2023-12-22 20:08:46 +0700

    Tests added

---
+++ test/dummy/app/models/missing_model.rb
@@ -0,0 +1,2 @@
+class MissingModel < ApplicationRecord
+end
--- test/rails_model_load_hook_test.rb
+++ test/rails_model_load_hook_test.rb
@@ -1,7 +1,57 @@
 require "test_helper"
+require 'minitest/autorun'
 
-class RailsModelLoadHookTest < ActiveSupport::TestCase
-  test "it has a version number" do
-    assert RailsModelLoadHook::VERSION
-  end
+describe RailsModelLoadHook do
+	let(:loaded)  { [] }
+	let(:on_load) { -> { loaded << _1 } }
+
+	before do
+		on_load = self.on_load
+
+		ActiveSupport.on_load :model_class do
+			on_load[self]
+		end
+	end
+
+	module SharedExamples
+		refine Minitest::Spec.singleton_class do
+			def passing
+				it 'passes the hook' do
+					_(loaded).must_include model
+				end
+			end
+
+			def skipping
+				it 'skips the hook' do
+					_(loaded).wont_include model
+				end
+			end
+		end
+	end
+
+	using SharedExamples
+
+	describe 'with existing models' do
+		let(:model) { DummyModel.tap { _1.new } }
+
+		passing
+	end
+
+	describe 'with inherited models' do
+		let(:model) { InheritedModel.tap { _1.new } }
+
+		passing
+	end
+
+	describe 'with ApplicationRecord' do
+		let(:model) { ApplicationRecord }
+
+		skipping
+	end
+
+	describe 'with models lacking DB tables' do
+		let(:model) { MissingModel }
+
+		skipping
+	end
 end
